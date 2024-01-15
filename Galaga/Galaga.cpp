4d13b21b80c23611ad67817e67cdcf9dbb00e41e
kykim9050 김경윤 <kykim9050@naver.com>
2024-01-15 14:55:47 +0900

    김경윤 : ConsoleEngine, Galaga 프로젝트 선생님 파일로 업데이트

--- Galaga/Galaga.cpp
+++ Galaga/Galaga.cpp
@@ -3,25 +3,39 @@
 
 #include <iostream>
 #include <ConsoleEngine/ConsoleScreen.h>
+#include <ConsoleEngine/EngineDebug.h>
+#include <list>
+#include <map>
+#include "Player.h"
+#include "Bullet.h"
+#include "Monster.h"
 
+#include "ContentsEnum.h"
+#include <ConsoleEngine/EngineCore.h>
 
 int main()
 {
+    EngineCore EngineCore;
 
-    ConsoleScreen NewScreen = ConsoleScreen();
+    EngineCore.Init({ 20, 20 });
 
-    NewScreen.Test();
 
-    std::cout << "Hello World!\n";
+    {
+        // Player* NewObject = new Player();
+        Player* NewObject = EngineCore.CreateObject<Player>(GalagaUpdateType::Player, GalagaRenderType::Player);
+        NewObject->SetPos({ EngineCore.Screen.GetScreenX() / 2, EngineCore.Screen.GetScreenY() - 2 });
+    }
+
+    {
+        const int MonsterCount = EngineCore.Screen.GetScreenX() / 2;
+        for (int i = 0; i < MonsterCount; i++)
+        {
+            Monster* NewObject = EngineCore.CreateObject<Monster>(GalagaUpdateType::Monster, GalagaRenderType::Monster);
+            NewObject->SetPos({ i, 0 });
+            NewObject->SetRenderChar('&');
+        }
+    }
+
+    EngineCore.Start();
+
 }
-
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
